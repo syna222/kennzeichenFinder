639d954cc806f24ee8d2dd2668f9212cdbe714da
Sarah <sarah.liess@hotmail.de>
2023-03-02 20:47:07 +0100

    1000 years later

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import axios from "axios";
-import SearchPage from "./Components/SearchPage";
+import SearchPage from "./Components/SearchPageNew";
 import Listen from "./Components/Listen";
 import AZ from "./Components/AZ";
 import NaBu from "./Components/NaBu";
@@ -13,10 +13,10 @@ import { NavLink, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [ chosenKFZ, setChosenKFZ ] = useState([]); //das hier in SearchPage setzen und an InfoPage passen
-  const [ allKFZ, setAllKFZ ] = useState([]); //passed down to AZ component
-  const [ KFZSortedBL, setKFZSortedBL ] = useState([]); //sorting by Bundesland to pass down to NaBu component
-  const [ bundesländer, setBundesländer ] = useState([]);
+  const [chosenKFZ, setChosenKFZ] = useState(""); //das hier in SearchPage setzen und an InfoPage passen
+  const [allKFZ, setAllKFZ] = useState([]); //passed down to AZ component
+  const [KFZSortedBL, setKFZSortedBL] = useState([]); //sorting by Bundesland to pass down to NaBu component
+  const [bundesländer, setBundesländer] = useState([]);
 
   let listeBundesländer = [
     { name: "Baden-Württemberg", kfzs: [] },
@@ -53,7 +53,9 @@ export default function App() {
   useEffect(() => {
     //alle Kennzeichen nach Bundesland (u. innerhalb alphabetisch) fetchen + als state variable speichern:
     axios
-      .get(`https://kennzeichenapi.onrender.com/kennzeichen/?sortkfz=true&sortbu=true`)
+      .get(
+        `https://kennzeichenapi.onrender.com/kennzeichen/?sortkfz=true&sortbu=true`
+      )
       .then((res) => {
         setKFZSortedBL(res.data);
       }) //console.log(res.data);
@@ -63,13 +65,16 @@ export default function App() {
       });
   }, []);
 
-  useEffect(() => {   //läuft erst, wenn KFZSortedBL in useEffect() davor gesetzt ist.
+  useEffect(() => {
+    //läuft erst, wenn KFZSortedBL in useEffect() davor gesetzt ist.
     //listeBundesländer durchlaufen, für jedes die jeweiligen aus KFZSortedBL rausziehen und in kfzs-feld speichern:
     listeBundesländer.forEach((bu, i) => {
       const bundesland = bu.name;
       //console.log("bundesland ist:", bundesland);
       //console.log("KFZSortedBL ist:", KFZSortedBL)
-      const results = KFZSortedBL.filter((obj) => obj.Bundesland === bundesland);
+      const results = KFZSortedBL.filter(
+        (obj) => obj.Bundesland === bundesland
+      );
       //console.log(results);
       //bu.kfzs = results; das funktioniert nicht, weil bu.kfzs nicht der tatsächliche Objekt-Teil ist!!
       listeBundesländer[i].kfzs = results;
@@ -81,23 +86,40 @@ export default function App() {
 
   //console.log("TEST aus App.js/ KFZSortedBL is: ", KFZSortedBL);
 
-
-
   return (
     <div className="App">
       <nav>
-        <NavLink className="nav-element" to="/suche">SUCHE</NavLink>
-        <NavLink className="nav-element" to="/listen">LISTE(N)</NavLink>
-        <NavLink className="nav-element" to="/karte">KARTE</NavLink>
-        <NavLink className="nav-element" to="/quiz">QUIZ</NavLink>
+        <NavLink className="nav-element" to="/suche">
+          SUCHE
+        </NavLink>
+        <NavLink className="nav-element" to="/listen">
+          LISTE(N)
+        </NavLink>
+        <NavLink className="nav-element" to="/karte">
+          KARTE
+        </NavLink>
+        <NavLink className="nav-element" to="/quiz">
+          QUIZ
+        </NavLink>
       </nav>
 
       <Routes>
-        <Route path="/suche" element={<SearchPage setChosenKFZ={setChosenKFZ} chosenKFZ={chosenKFZ} />}/>
-        <Route path="/aktuelles_kfz"element={<InfoPage chosenKFZ={chosenKFZ} />}/>
+        <Route
+          path="/suche"
+          element={
+            <SearchPage setChosenKFZ={setChosenKFZ} chosenKFZ={chosenKFZ} />
+          }
+        />
+        <Route
+          path="/aktuelles_kfz"
+          element={<InfoPage chosenKFZ={chosenKFZ} />}
+        />
         <Route path="/listen" element={<Listen />} />
         <Route path="/listen/a-z" element={<AZ allKFZ={allKFZ} />} />
-        <Route path="/listen/nabu" element={<NaBu bundesländer={bundesländer} />}/>
+        <Route
+          path="/listen/nabu"
+          element={<NaBu bundesländer={bundesländer} />}
+        />
         <Route path="/karte" element={<Karte />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/quiz/kfz_stla" element={<QuizStLa allKFZ={allKFZ} />} />
--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,123 +1,201 @@
-import axios from 'axios';
-import { useState, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export default function SearchPage({setChosenKFZ, chosenKFZ}){
+export default function SearchPage({ setChosenKFZ, chosenKFZ }) {
+  console.log("chosenkfz", chosenKFZ);
 
-    console.log('chosenkfz', chosenKFZ)
+  const checkboxRef = useRef();
 
-    const checkboxRef = useRef();
+  const [changed, setChanged] = useState(false); //for conditional rendering
+  const [checked, setChecked] = useState(false);
+  const [userKennzeichen, setUserKennzeichen] = useState([]);
+  const [insideUserKennzeichen, setInsideUserKennzeichen] = useState(false);
 
-    const [ changed, setChanged ] = useState(false);     //for conditional rendering
-    const [ checked, setChecked ] = useState(false); 
+  //1. access chosenKFZ based on user input (from props)
+  //2. fetch user's Gesehene_Kennzeichen
+  //3. check if chosenKFZ._id is in Gesehene_Kennzeichen
+  //4. if yes, set checkbox to checked
+  //5. if no, set checkbox to unchecked
 
-    function checkKennzeichenForUser(){
-        //get alle Gesehene_Kennzeichen from User, dann res.data.Gesehene_Kennzeichen durchlaufen + checken ob chosenKFZ._id drin ist
-        let gesehene_kfz = [];
-        let isInList = false;
-        //ACHTUNG! userid wird hier hardgecodet auf Viola! später ändern!
-        const user_id = "63ff6f9c858ac063472de5b7"
-        const URL = `https://kennzeichenapi.onrender.com/users/${user_id}`;
-        axios.get(URL)
-            .then(res => {
-                gesehene_kfz = res.data.Gesehene_Kennzeichen;
-                console.log(gesehene_kfz); 
-            })
-            .catch(err => console.log(err))
-        //check if chosenKFZ._id in res.data.Gesehene_Kennzeichen (only if chosenKFZ is set yet!):
-        if(chosenKFZ){      //HÄNGT ES HIER??!
-            // console.log('id',chosenKFZ._id);
-            const doesExist = (kfz) => kfz._id === chosenKFZ._id;
-            isInList = gesehene_kfz.some(doesExist);
-            console.log("isInList from checkKennzeichenForUser(): ", isInList);
-        }
-        return isInList;
+  //   const user_id = "63ff6f9c858ac063472de5b7";
+  //   const URL = `https://kennzeichenapi.onrender.com/users/${user_id}`;
 
+  //   useEffect(() => {
+  //     const checkKennzeichenForUser = async () => {
+  //       try {
+  //         const data = await axios.get(URL);
+  //         console.log("data", data.data.Gesehene_Kennzeichen);
+  //         setUserKennzeichen(data.data.Gesehene_Kennzeichen);
+  //       } catch (err) {
+  //         console.log(err);
+  //       }
+  //     };
+  //     checkKennzeichenForUser();
+  //   }, []);
 
+  //   if (chosenKFZ && userKennzeichen.length) {
+  //     console.log("yes");
+  //   }
+
+  // function checkKennzeichenForUser(){
+  //     //get alle Gesehene_Kennzeichen from User, dann res.data.Gesehene_Kennzeichen durchlaufen + checken ob chosenKFZ._id drin ist
+  //     let gesehene_kfz = [];
+  //     let isInList = false;
+  //     //ACHTUNG! userid wird hier hardgecodet auf Viola! später ändern!
+  //     const user_id = "63ff6f9c858ac063472de5b7"
+  //     const URL = `https://kennzeichenapi.onrender.com/users/${user_id}`;
+  //     axios.get(URL)
+  //         .then(res => {
+  //             gesehene_kfz = res.data.Gesehene_Kennzeichen;
+  //             console.log(gesehene_kfz);
+  //         })
+  //         .catch(err => console.log(err))
+  //     //check if chosenKFZ._id in res.data.Gesehene_Kennzeichen (only if chosenKFZ is set yet!):
+  //     if(chosenKFZ){      //HÄNGT ES HIER??!
+  //         // console.log('id',chosenKFZ._id);
+  //         const doesExist = (kfz) => kfz._id === chosenKFZ._id;
+  //         isInList = gesehene_kfz.some(doesExist);
+  //         console.log("isInList from checkKennzeichenForUser(): ", isInList);
+  //     }
+  //     return isInList;
+
+  // }
+
+  async function checkKennzeichenForUser() {
+    let gesehene_kfz = [];
+    let isInList = false;
+    const user_id = "63ff6f9c858ac063472de5b7";
+    const URL = `https://kennzeichenapi.onrender.com/users/${user_id}`;
+
+    try {
+      const res = await axios.get(URL);
+      gesehene_kfz = res.data.Gesehene_Kennzeichen;
+      //   console.log("gesehene_kfz: ", gesehene_kfz);
+
+      //   console.log("chosenKFZ: ", chosenKFZ);
+      if (chosenKFZ) {
+        const doesExist = (kfz) => kfz._id === chosenKFZ._id;
+        isInList = gesehene_kfz.some(doesExist);
+        // console.log("isInList from checkKennzeichenForUser(): ", isInList);
+        setInsideUserKennzeichen(isInList);
+      }
+    } catch (err) {
+      console.log(err);
     }
 
-    console.log('id',chosenKFZ._id);
+    return isInList;
+  }
+  console.log("id", chosenKFZ._id);
+  console.log("yes or nooooo", insideUserKennzeichen);
 
-    function handleChange(e){
-        //fetch from API:
-        const input = e.target.value;
-        const URL = `https://kennzeichenapi.onrender.com/kennzeichen/${input}`;
-        if(input){
-            axios.get(URL)
-            .then(res => {
-                //console.log(res.data[0]); 
-                setChosenKFZ(res.data[0]); 
-                setChanged(true);
-                //hier muss checkbox gesetzt werden, falls chosenKFZ._id schon in user's Gesehene_Kennzeichen:
-                const isInList = checkKennzeichenForUser();
-                console.log("ist schon in Gesehene_Kennzeichen des Users:", isInList);
-            })
-            .catch(err => console.log(err))
-        }
-        else{
-            setChanged(false);
-            //checkbox unchecken:
-            setChecked(false);
-            checkboxRef.current.checked = false;
-        }
+  async function handleChange(e) {
+    const input = e.target.value;
+    const URL = `https://kennzeichenapi.onrender.com/kennzeichen/${input}`;
+
+    if (input) {
+      try {
+        const res = await axios.get(URL);
+        setChosenKFZ(res.data[0]);
+        setChanged(true);
+
+        // Check if the chosenKFZ._id is already in the user's Gesehene_Kennzeichen:
+        const isInList = await checkKennzeichenForUser();
+        console.log("ist schon in Gesehene_Kennzeichen des Users:", isInList);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      setChanged(false);
+      checkboxRef.current.checked = false;
     }
+  }
 
-    function handleCheck(){
-        //wird zu seinem Gegenteil, damit bei setChecked(true) und input defined das Kfz gespeichert wird...
-        if(checked === false){
-            setChecked(true);
-            console.log("checked is true");
-            //KfZ wird gespeichert via put-request:
-            const kfz_id = chosenKFZ._id;
-            //ACHTUNG! userid wird hier hardgecodet auf Viola! später ändern!
-            const user_id = "63ff6f9c858ac063472de5b7"
-            const URL = `https://kennzeichenapi.onrender.com/users/${user_id}/addkennzeichen`;
-            //console.log(URL);
-            //post to new url:
-            axios.put(URL, {
-                kennzeichenId: kfz_id       //muss kennzeichenId heißen, weil in backend so destrukturiert angegeben
-            })
-              .then(function (response) {
-                //console.log(response);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
+  //   function handleChange(e) {
+  //     //fetch from API:
+  //     const input = e.target.value;
+  //     const URL = `https://kennzeichenapi.onrender.com/kennzeichen/${input}`;
+  //     if (input) {
+  //       axios
+  //         .get(URL)
+  //         .then((res) => {
+  //           //console.log(res.data[0]);
+  //           setChosenKFZ(res.data[0]);
+  //           setChanged(true);
+  //           //hier muss checkbox gesetzt werden, falls chosenKFZ._id schon in user's Gesehene_Kennzeichen:
+  //           const isInList = checkKennzeichenForUser();
+  //           console.log("ist schon in Gesehene_Kennzeichen des Users:", isInList);
+  //         })
+  //         .catch((err) => console.log(err));
+  //     } else {
+  //       setChanged(false);
+  //       //checkbox unchecken:
+  //       setChecked(false);
+  //       checkboxRef.current.checked = false;
+  //     }
+  //   }
 
-        }
-        else{
-            setChecked(false);
-            console.log("checked is false");
-            //KfZ wird aus Speicher gelöscht via put-request:
-            const kfz_id = chosenKFZ._id;
-            //ACHTUNG! userid wird hier hardgecodet auf Viola! später ändern!
-            const user_id = "63ff6f9c858ac063472de5b7"
-            const URL = `https://kennzeichenapi.onrender.com/users/${user_id}/removekennzeichen`;
-            //console.log(URL);
-            //post to new url:
-            axios.put(URL, {
-                kennzeichenId: kfz_id       //muss kennzeichenId heißen, weil in backend so destrukturiert angegeben
-            })
-              .then(function (response) {
-                //console.log(response);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-        }
-
-
-
+  function handleCheck() {
+    //wird zu seinem Gegenteil, damit bei setChecked(true) und input defined das Kfz gespeichert wird...
+    if (checked === false) {
+      setChecked(true);
+      console.log("checked is true");
+      //KfZ wird gespeichert via put-request:
+      const kfz_id = chosenKFZ._id;
+      //ACHTUNG! userid wird hier hardgecodet auf Viola! später ändern!
+      const user_id = "63ff6f9c858ac063472de5b7";
+      const URL = `https://kennzeichenapi.onrender.com/users/${user_id}/addkennzeichen`;
+      //console.log(URL);
+      //post to new url:
+      axios
+        .put(URL, {
+          kennzeichenId: kfz_id, //muss kennzeichenId heißen, weil in backend so destrukturiert angegeben
+        })
+        .then(function (response) {
+          //console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      setChecked(false);
+      console.log("checked is false");
+      //KfZ wird aus Speicher gelöscht via put-request:
+      const kfz_id = chosenKFZ._id;
+      //ACHTUNG! userid wird hier hardgecodet auf Viola! später ändern!
+      const user_id = "63ff6f9c858ac063472de5b7";
+      const URL = `https://kennzeichenapi.onrender.com/users/${user_id}/removekennzeichen`;
+      //console.log(URL);
+      //post to new url:
+      axios
+        .put(URL, {
+          kennzeichenId: kfz_id, //muss kennzeichenId heißen, weil in backend so destrukturiert angegeben
+        })
+        .then(function (response) {
+          //console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
+  }
 
-    return(
+  return (
     <>
-        <input id="kennzeichen-suchfeld" type="text" onChange={handleChange}/>
-        <input ref={checkboxRef} type="checkbox" onChange={handleCheck} />
-        <div id="Ort_Stadt">Stadt/Ort: {changed && chosenKFZ && <Link to="/aktuelles_kfz">{chosenKFZ.Stadt_Ort}</Link>}</div>
-        <div id="Landkreis">Landkreis: {changed &&  chosenKFZ && chosenKFZ.Landkreis}</div>
-        <div id="bundesland">Bundesland: {changed &&  chosenKFZ && chosenKFZ.Bundesland}</div>
+      <input id="kennzeichen-suchfeld" type="text" onChange={handleChange} />
+      <input ref={checkboxRef} type="checkbox" onChange={handleCheck} />
+      <div id="Ort_Stadt">
+        Stadt/Ort:{" "}
+        {changed && chosenKFZ && (
+          <Link to="/aktuelles_kfz">{chosenKFZ.Stadt_Ort}</Link>
+        )}
+      </div>
+      <div id="Landkreis">
+        Landkreis: {changed && chosenKFZ && chosenKFZ.Landkreis}
+      </div>
+      <div id="bundesland">
+        Bundesland: {changed && chosenKFZ && chosenKFZ.Bundesland}
+      </div>
     </>
-    );
-
+  );
 }
